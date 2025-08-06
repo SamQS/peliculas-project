1bef7a49cc7f9f313219776227a15109ac6a83ce
SamQS <diegodsqs2@gmail.com>
2025-08-06 14:45:26 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://SamQS.github.io/peliculas-react/',
+  base: 'https://SamQS.github.io/peliculas-project/',
 })
