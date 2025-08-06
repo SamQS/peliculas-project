2e97e717bbba5410f2ea96103c7ac90e6ffe71ae
SamQS <diegodsqs2@gmail.com>
2025-08-06 16:11:29 -0500

    update2

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,15 +1,15 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {HashRouter, Routes, Route} from "react-router-dom"
 import { LadingPage } from "../pages/LadingPage";
 import { MovieDetails } from "../pages/MovieDetails";
 
 export function MyRoutes() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<LadingPage />} />
                 <Route path="/movie/:id" element={<MovieDetails />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
 
     )
 }
